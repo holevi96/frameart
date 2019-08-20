10379716d72fba05352499a6cdce632a1ddcb521
H.Levi <holevi96@gmail.com>
2019-08-20 22:06:39 +0200

    base animation

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style.css">
+    <title>FrameArt Hungary</title>
+</head>
+<body>
+<div class="video">
+    <video class="fullscreen" src="assets/The-Slow-Dock.mp4"  autoplay />
+</div>
+<div class="columns">
+    <div class="col"></div>
+    <div class="col"></div>
+    <div class="col"></div>
+</div>
+
+</body>
+</html>
